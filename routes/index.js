7aebeef77cda01e03dd94f6b9f13cdd3ee375e54
abhirammadugula93 <abhirammadugula93@gmail.com>
2019-12-05 16:17:18 -0600

    Updated with registration pages and basic views along with basic dashboard

--- routes/index.js
+++ routes/index.js
@@ -31,8 +31,24 @@ router.get("/", (req, res) => {
   res.render("login.html");
 });
 
+router.get("/home", (req, res) => {
+  res.render("index.html");
+});
+
+router.get("/about", (req, res) => {
+  res.render("about.html");
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contact.html");
+});
+
 router.post("/login", (req, res) => {
   res.render("index.html");
+});
+
+router.get("/logout", (req, res) => {
+  res.render("login.html");
 });
 
 module.exports = router;
